47e1a047a87364f28fd9765e4ddefdf132d3bd7c
Tay Puri <taypuri@Tays-MacBook-Air.local>
2025-10-03 16:45:55 -0300

    feat: add character-sheet components index

---
+++ apps/web/src/components/character-sheet/index.ts
@@ -0,0 +1,6 @@
+export { AbilityScores } from './ability-scores';
+export { CharacterHeader } from './character-header';
+export { CoreStats } from './core-stats';
+export { SavingThrows } from './saving-throws';
+export { Skills } from './skills';
+export { StatBox } from './stat-box';
